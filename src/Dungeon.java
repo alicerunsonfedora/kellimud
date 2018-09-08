52cbbe99e87eb1daa1316389c22391b2952fe741
Marquis Kurt <software@marquiskurt.net>
2018-09-08 12:26:41 -0400

    Add properties to Dungeon, Room classes

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
 
+/**
+ * The base area for creating a set of rooms in a grid.
+ * @author Marquis Kurt
+ */
 public class Dungeon {
+	private ArrayList<Integer> rooms;
 
+	/**
+	 * Instantiate the object
+	 */
 	public Dungeon() {
 		// TODO Auto-generated constructor stub
 	}
--- src/Room.java
+++ src/Room.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.Random;
 
+/**
+ * Area for player to enter and grab loot or defeat a monster
+ * @author Marquis Kurt
+ */
 public class Room {
+	private int ID;
+	private ArrayList<Item> loot;
+	private Boolean monsterInRoom;
+	private int[] doors;
 
+	/**
+	 * Instantiate the object
+	 */
 	public Room() {
 		// TODO Auto-generated constructor stub
 	}
