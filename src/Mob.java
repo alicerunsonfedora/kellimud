458726c21d3b491d7e9f03a317008e6429e68f9a
TheDancingPotato <43046230+TheDancingPotato@users.noreply.github.com>
2018-09-08 13:04:39 -0400

    Created Mob and Player Basic Class

--- src/Mob.java
+++ src/Mob.java
@@ -1,8 +1,28 @@
 
 public class Mob {
 
-	public Mob() {
-		// TODO Auto-generated constructor stub
+	private int lvl,health,attack,defense;
+	
+	public Mob(int level) 
+	{
+		lvl = 1;
+		health = 10*lvl;
+		attack = lvl;
+		defense = lvl;
 	}
-
+	
+	public void attack()
+	{
+		
+	}
+	
+	public void die()
+	{
+		
+	}
+	
+	public void move()
+	{
+		
+	}
 }
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,78 @@
 
 public class Player {
-
-	public Player() {
-		// TODO Auto-generated constructor stub
+	
+	private String name,playerclass;
+	private int lvl,health,attack,defense,exp;
+	private Item hand,armor;
+	private Item[] backpack = new Item[10];
+	private int flag;
+	
+	public Player(String aname,String aclass) 
+	{
+		name = aname;
+		playerclass = aclass;
+		lvl = 1;
+		health = 10;
+		attack = 1;
+		defense = 0;
+		exp = 0;
+		flag = 0;
 	}
+	
+	public void die()
+	{
+		
+	}
+	
+	public String manage_inventory(String action,String item)
+	{
+		int i;
+		Item temp;
+		
+		if (action.equals("pick up"))
+		{
+			backpack[flag] = finditem(item);
+		}
+		
+		if (action.equals("drop"))
+		{
+			for(i=0;i<10;i++)
+			{
+				if(backpack[i].name.equals(item))
+				{
+					temp = backpack[i];
+					backpack[i] = backpack[flag-1];
+					backpack[flag-1] = temp;
+					backpack[flag-1] = null;
+					break;
+				}
+			}
+		}
+		
+		if(action.equals("pick up"))
+		{
+			return "You picked up " + item;
+		}
+		else
+		{
+			return "You dropped " + item;
+		}
+	}
+	
+	public void attack()
+	{
+		
+	}
+	
+	public void exit()
+	{
+		
+	}
+	
+	public void move()
+	{
+		
+	}
+	
 
 }
