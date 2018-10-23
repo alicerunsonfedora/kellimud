9fd5add651a8cdc40b60c64dc80afb65a92acacd
Marquis Kurt <software@marquiskurt.net>
2018-10-23 14:48:23 -0400

    Move everything to a command interpreter

---
+++ src/GUIInterpreter.java
@@ -0,0 +1,11 @@
+public class GUIInterpreter {
+	
+	//TODO: implement GUI commands that match Command counterparts
+	public void interpret(String command, Player who, Dungeon where) {
+		
+	}
+	
+	public GUIInterpreter() {
+		
+	}
+}
